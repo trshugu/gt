5c967aa1b85e6b4d61ec151d4b878413dac8961e
arr <go.rupe+4@gmail.com>
2013-07-08 12:40:29 +0900

    getset

--- tmp.groovy
+++ tmp.groovy
@@ -1,1 +1,20 @@
+/*
+*/
+
+
+/*
+// get set確認
+class Pojo {
+    def name
+}
+def pojo = new Pojo(name:"名前")
+println pojo.getName() // getName()が生成されている
+println pojo.name // getName()が呼ばれる
+*/
+
+/*
+// groovyテスト
 println "Hell, World"
+*/
+
+
