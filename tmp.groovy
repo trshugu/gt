55b8b6fad63ea319ff6bb2b4bcab8e4d7526b7f6
arr <go.rupe+4@gmail.com>
2013-08-02 19:22:11 +0900

    suger

--- tmp.groovy
+++ tmp.groovy
@@ -2,9 +2,211 @@
 */
 
 
+
+
+
+
+/*
+// GroovyBean
+class Book
+{
+  String title  // プロパティの宣言
+}
+
+def groovyBook = new Book()
+
+// 明示的なメソッド呼び出しによるプロパティの利用
+groovyBook.setTitle('Groovy world')
+assert groovyBook.getTitle() == 'Groovy world'
+
+// Groovyによるショートカットを使ったプロパティの利用
+groovyBook.title = 'No Groovy, No Life'
+assert groovyBook.title == 'No Groovy, No Life'
+*/
+
+/*
+Groovyでは以下のパッケージが暗黙的にインポート済み
+java.io.*
+java.lang.*
+java.math.BigDecimal
+java.math.BigInteger
+java.net.*
+java.util.*
+groovy.lang.*
+groovy.util.* 
+*/
+
+/*
+[1, 2, 3].each { entry -> println entry }
+[4, 5, 6].each { println it } // パラメータ省略時はitで参照できる
+[a: 100, b: 200, c: 300].each { key, value ->
+  println "${key}=${value}"
+}
+*/
+
+
+/*
+// 範囲
+def x = 1..10
+assert x.contains(5)
+assert x.contains(15) == false
+assert x.size() == 10
+assert x.from == 1
+assert x.to == 10
+assert x.reverse() == 10..1
+
+def y = 1..<10
+assert y.from == 1
+assert y.to == 9
+*/
+
+
+/*
+// マップも[]で囲み、「キー:値」の形で内容を示し、カンマで区切る
+def http = [
+  100 : 'CONTINUE',
+  200 : 'OK',
+  400 : 'BAD REQUEST' ]
+assert http[200] == 'OK'
+
+http[500] = 'INTERNAL SERVER ERROR'
+assert http.size() == 4
+*/
+
+/*
+// リスト、マップ、範囲
+def roman = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII'] // ローマ数字のリスト
+assert roman[4] == 'IV' // リストへのアクセス
+
+roman[8] = 'VIII' // リストの拡大
+assert roman.size() == 9
+*/
+
+/*
+// Boolean値は自明
+assert true
+assert !false
+
+// Matcherはマッチすること
+assert ('a' =~ /./)
+assert !('a' =~ /b/)
+
+// コレクションは空ではないこと
+assert [1]
+assert ![]
+
+// マップは空ではないこと
+assert ['a': 1]
+assert ![:]
+
+// 文字列は空ではないこと
+assert 'a'
+assert !''
+
+// 数値（どの型も）はゼロではないこと
+assert 1
+assert 1.1
+assert 1.2f
+assert 1.3g
+assert 2L
+assert 3G
+assert !0
+
+// その他は、すべてnullではないこと
+assert new Object()
+assert !null
+*/
+
+
+
+/*
+// returnが省略可能 省略した場合、最後に評価された値が返却される
+def method()
+{
+  def a = 10
+  def b = 20
+  a + b
+}
+assert method() == 30
+*/
+
+/*
+// try-catch
+try
+{
+  // 処理
+}
+catch(e)
+{
+  // 例外e発生時処理
+}
+finally
+{
+  // 最終処理
+}
+*/
+
+/*
+switch(3)
+{
+  case 1 : assert false; break
+  case 3 : assert true;  break
+  default: assert false
+}
+*/
+
+/*
+for (i = 0; i < 10; i++) // 昔ながらのfor
+{
+  println i
+}
+
+def clinks = 0
+for (remainingGuests in 0..9) // 範囲に対するfor
+{
+  clinks += remainingGuests 
+}
+println clinks
+
+def list = [0, 1, 2, 3, 4, 5]
+for (j in list) // リストに対するfor
+{
+  assert j == list[j]
+}
+*/
+
+
+/*
+def i = 0
+while (i < 10)
+{
+  i++
+}
+assert i == 10
+*/
+
+/*
+if (false) assert false // 単一行のif
+
+if (null) // nullは偽
+{
+  assert false
+}
+  else
+{
+  assert true
+}
+*/
+
+/*
+// assertion 真の間は実行しても何も起こらない
+def a = 10
+def b = 40
+assert 30 == a + b, '足したら30のはず'
+*/
+
 /*
 // sbと+の速度の比較で多いほう groovyだと2倍遅かった
-
 // +の場合
 long start = System.currentTimeMillis();
 int cnt = 0;
@@ -69,6 +271,7 @@ long isb = endsb - startsb;
 System.out.println(i);
 System.out.println(isb);
 */
+
 
 
 /*
